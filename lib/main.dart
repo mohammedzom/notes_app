f65520bdc4f02ecc6a2ec1821215d948b8e4507e
Mohammed Z <mohammedzomlot2@gmail.com>
2023-07-07 15:16:17 +0200

    add hive

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:notes_app/views/notes_view.dart';
 
-void main() {
+void main() async {
   runApp(const NotesApp());
+
+  await Hive.initFlutter();
 }
 
 class NotesApp extends StatelessWidget {
