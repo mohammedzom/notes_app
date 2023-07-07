fbde23577382d92aa2b3c87efb10b03051e9e06d
Mohammed Z <mohammedzomlot2@gmail.com>
2023-07-07 19:59:37 +0200

    save

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:notes_app/consts.dart';
+import 'package:notes_app/cubit/add_note_cubit/add_note_cubit.dart';
+import 'package:notes_app/models/note_model.dart';
+import 'package:notes_app/simple_bloc_observer.dart';
 import 'package:notes_app/views/notes_view.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() async {
   runApp(const NotesApp());
 
   await Hive.initFlutter();
 
-  await Hive.openBox(kNotesBox);
+  Bloc.observer = SimpleBlocObserver();
+  Hive.registerAdapter(NoteModelAdapter());
+  await Hive.openBox<NoteModel>(kNotesBox);
 }
 
 class NotesApp extends StatelessWidget {
